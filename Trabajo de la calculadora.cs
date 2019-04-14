691f49b9332e1647a7e39343c8f9266819780c24
Victor-dani <47483759+Victor-dani@users.noreply.github.com>
2019-04-13 23:11:59 -0400

    Add files via upload
    Link de la presentación   https://es.slideshare.net/DanielAlvarez471/clipboards/my-clips
    Link del video                  https://youtu.be/o4CWRRXohG0

---
+++ Trabajo de la calculadora.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp29
+{
+    public partial class Form1 : Form
+    {
+        double primero;
+        double segundo;
+        Double resultado;
+        string operación;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+                
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pantalla.Text = pantalla.Text + "4";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            pantalla.Text = pantalla.Text + "0";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pantalla.Text = pantalla.Text + "1";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            pantalla.Text = pantalla.Text + "2";
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            pantalla.Text = pantalla.Text + "3";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            pantalla.Text = pantalla.Text + "5";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            pantalla.Text = pantalla.Text + "6";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pantalla.Text = pantalla.Text + "7";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            pantalla.Text = pantalla.Text + "8";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            pantalla.Text = pantalla.Text + "9";
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            pantalla.Text = pantalla.Text + ".";
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            operación = "+";
+            primero = double.Parse(pantalla.Text);
+            pantalla.Clear();
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            operación = "-";
+            primero = double.Parse(pantalla.Text);
+            pantalla.Clear();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            operación = "*";
+            primero = double.Parse(pantalla.Text);
+            pantalla.Clear();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            operación = "/";
+            primero = double.Parse(pantalla.Text);
+            pantalla.Clear();
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            segundo = double.Parse(pantalla.Text);
+
+            switch(operación){
+                case "+":
+                    resultado = primero + segundo;
+                    pantalla.Text = resultado.ToString();
+
+                    break;
+                case "-":
+                    resultado = primero - segundo;
+                    pantalla.Text = resultado.ToString();
+
+                    break;
+                case "*":
+                    resultado = primero * segundo;
+                    pantalla.Text = resultado.ToString();
+
+                    break;
+                case "/":
+                    resultado = primero / segundo;
+                    pantalla.Text = resultado.ToString();
+
+                    break;
+
+            }
+    }
+
+        private void btnlimpiar_Click(object sender, EventArgs e)
+        {
+            pantalla.Clear();
+        }
+    }
+}
+
